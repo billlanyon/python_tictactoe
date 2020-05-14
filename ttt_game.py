fcac52dd897078533eb5b6f187256cf3198d3b7c
Basil <basilbibi@hotmail.com>
2020-05-14 18:26:49 +0100

    BBB: Adds first tests for Game class. Removes pycharm project files.

--- ttt_game.py
+++ ttt_game.py
@@ -11,29 +11,29 @@ class Game:
         self.board = [[' ', ' ', ' '],
                       [' ', ' ', ' '],
                       [' ', ' ', ' ']]
-        while True:
-            try:
-                self.initial_setup = input('Enter cells: > ')
-                if re.match("^[XO_]*$", self.initial_setup) and len(self.initial_setup) == 9:
-                    break
-                print('Please enter a 9 character string containing only X, O or _')
-            except ValueError as error:
-                print(error)
-                continue
-        while True:
-            try:
-                self.player = input('Enter who plays first, X or O: > ')
-                self.player = self.player.upper()
-                if re.match("^[XO]$", self.player):
-                    break
-                print('Please enter either an X or an O')
-            except ValueError as error:
-                print(error)
-                continue
-        self.player = self.player.upper()
-        for x in range(3):
-            for y in range(3):
-                self.board[x][y] = self.initial_setup[x * 3 + y]
+        # while True:
+        #     try:
+        #         self.initial_setup = input('Enter cells: > ')
+        #         if re.match("^[XO_]*$", self.initial_setup) and len(self.initial_setup) == 9:
+        #             break
+        #         print('Please enter a 9 character string containing only X, O or _')
+        #     except ValueError as error:
+        #         print(error)
+        #         continue
+        # while True:
+        #     try:
+        #         self.player = input('Enter who plays first, X or O: > ')
+        #         self.player = self.player.upper()
+        #         if re.match("^[XO]$", self.player):
+        #             break
+        #         print('Please enter either an X or an O')
+        #     except ValueError as error:
+        #         print(error)
+        #         continue
+        # self.player = self.player.upper()
+        # for x in range(3):
+        #     for y in range(3):
+        #         self.board[x][y] = self.initial_setup[x * 3 + y]
 
     def display(self):
         """
